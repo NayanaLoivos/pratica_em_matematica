71cc13ce57da2ae1c69ea52f12394effafe35990
Nayana Loivos <loivosdev@gmail.com>
2023-08-16 19:58:51 -0300

    my studies in percentage

---
+++ matematica/#1-conjuntos-e-operacoes-basicas/#6porcentagem.js
@@ -0,0 +1,54 @@
+//Porcentagem assim como fração é uma forma de termos uma medida relativa de algo.
+    //Exemplo, se vemos que o PIB do brasil está previsto para crescer 3% nesse ano, 
+    //estamos falando implicitamente que é 3% em relação ao valor anterior do ano passado.
+    //Só iremos extrair um significado absoluto desse exemplo quando tivermos o número 
+    //de fato de quanto foi o PIB ano passado. Por ser uma medida relativa 3% do PIB do Brasil, 
+    //será diferente de 3% do PIB da China e assim sucessivamente a outros Países.
+
+    //Outro exemplo, supondo que estamos querendo saber quanto é o desconto 10% de um produto que custa R$100,00.
+    //10% de 100.
+    //Uma porcentagem pode ser vista como uma fração que tem o denominador 100, ou seja, a fração que
+    //representa 10% nesse caso é: 10/100, então 10% de 100 reais é pegar essa fração 10/100 e
+    //multiplicar pelo valor que queremos verificar, ou seja, 100. Portanto:
+    //10/100 * 100 = 10/1 * 1/1 = 10.
+    //Então 10% em cima de 100 reais é 10 reais, logo se seu desconto é de 10%,
+    //você tem 90 reais a pagar.
+
+    //Digamos que agora quero vender esse produto que comprei por R$90,00.
+    //E para vender temos imposto, então temos que calcular o valor o qual teremos
+    //que aumentar esse produto para passar a outro consumido. aumentaremos, a priore
+    //10%.
+    //10/100 * 90 = 9.
+    //Então repara que se aumentarmos um produto que custa R$90,00 em 10% no total 
+    //obteremos R$99,00 como valor final, ao passo que se dermos desconto num produto de R$100,00
+    //obteremos R$90,00.
+    //Conclusão: Se olhar para o 90 e aumentarmos 10% é diferente de olharmos para o 100 
+    //e descontar 10%. Por isso a porcentagem é uma medida totalmente relativa.
+
+//Como fração é uma razão entre dois números inteiros temos que:
+    //7,5% de 1100, 7,5% é 7,5/100 onde o numerador e denominador pode ser multiplicado 
+    //por 10 que resultado em 75/1000.
+    //Portanto, 7,5/100 equivale 75/1000 que é uma representação válida para essa fração,
+    //que simplificando teremos 3/40.
+    //Portanto 7,5% é equivalente a 3/40.
+    //Se olhar para a fração 7,5/100 vemos que temos que multiplicar 7,5 e dividir por 100, 
+    //então o próprio numerador está te dando o valor que se deve multiplicar e o denominador 
+    //o valor que você tem que dividir. E para todas as porcentagens será assim.
+    //Para tirar 7,5% de 1100 é mais fácil olhar que para 7,5/100 do que para 3/40.
+
+    //QUAL A OUTRA MANEIRA DE OLHAR?
+        //Se paramos para pensar se divirmos 1100 por 100 obteremos 11, e logo podemos fazer 7,5 * 11 = 82.5.
+        //Bem como poderiamos realizar a conta 7,5/100 * 1100 = 7,5 * 11 = 82.5.
+       
+
+
+
+
+
+
+
+
+
+
+
+
