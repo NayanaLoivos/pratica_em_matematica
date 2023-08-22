f7863f98a6e72dfd1a93b2a365ae7835eb3b6068
Nayana Loivos <loivosdev@gmail.com>
2023-08-22 14:22:17 -0300

    how I can be the best and worst player at the same time

--- matematica/#1-conjuntos-e-operacoes-basicas/#6porcentagem.js
+++ matematica/#1-conjuntos-e-operacoes-basicas/#6porcentagem.js
@@ -1,45 +1,141 @@
-//Porcentagem assim como fração é uma forma de termos uma medida relativa de algo.
+//REFLEXÃO: "Tudo pode ser fácil se você tem o direcinamento e os pré-requisitos corretos.
+//Fácil é uma distância do quão distância tal coisa está do seu conhecimento atual.
+//Se está muito distante você diz que não é fácil, se está pouco distante você diz que é fácil!"
+
+//O QUE É PORCENTAGEM?
+    //Podemos entender um valor em porcentagem como uma fração cujo denominador é 100 
+    //e o numerador é igual ao valor da porcentagem. Por exemplo, 10% corresponde a fração 10/100.
+    //Porcentagem assim como fração é uma forma de termos uma medida relativa de algo.
     //Exemplo, se vemos que o PIB do brasil está previsto para crescer 3% nesse ano, 
     //estamos falando implicitamente que é 3% em relação ao valor anterior do ano passado.
     //Só iremos extrair um significado absoluto desse exemplo quando tivermos o número 
-    //de fato de quanto foi o PIB ano passado. Por ser uma medida relativa 3% do PIB do Brasil, 
+    //de fato de quanto foi o PIB no ano passado. Por ser uma medida relativa 3% do PIB do Brasil, 
     //será diferente de 3% do PIB da China e assim sucessivamente a outros Países.
 
-    //Outro exemplo, supondo que estamos querendo saber quanto é o desconto 10% de um produto que custa R$100,00.
+    //Outro exemplo, supondo que estamos querendo saber quanto é o desconto de 10% de um produto que custa R$100,00.
     //10% de 100.
     //Uma porcentagem pode ser vista como uma fração que tem o denominador 100, ou seja, a fração que
-    //representa 10% nesse caso é: 10/100, então 10% de 100 reais é pegar essa fração 10/100 e
-    //multiplicar pelo valor que queremos verificar, ou seja, 100. Portanto:
+    //representa 10% nesse caso é 10/100, então 10% de 100 reais é pegar essa fração 10/100 e
+    //multiplicá-la pelo valor que queremos verificar, ou seja, 100. Portanto:
     //10/100 * 100 = 10/1 * 1/1 = 10.
     //Então 10% em cima de 100 reais é 10 reais, logo se seu desconto é de 10%,
     //você tem 90 reais a pagar.
 
-    //Digamos que agora quero vender esse produto que comprei por R$90,00.
-    //E para vender temos imposto, então temos que calcular o valor o qual teremos
-    //que aumentar esse produto para passar a outro consumido. aumentaremos, a priore
-    //10%.
-    //10/100 * 90 = 9.
-    //Então repara que se aumentarmos um produto que custa R$90,00 em 10% no total 
-    //obteremos R$99,00 como valor final, ao passo que se dermos desconto num produto de R$100,00
-    //obteremos R$90,00.
-    //Conclusão: Se olhar para o 90 e aumentarmos 10% é diferente de olharmos para o 100 
-    //e descontar 10%. Por isso a porcentagem é uma medida totalmente relativa.
+    //E SE APLICAMOS O INVERSO DO EXEMPLO ACIMA?
+        //Digamos agora que quero vender esse produto que comprei por R$90,00.
+        //E para vender temos impostos à pagar sobre a venda, portanto temos que calcular o valor o qual teremos
+        //que aumentar esse produto para passar a outro consumido. Aumentando 10% temos
+        //10/100 * 90 = 9.
+        //Repara que se aumentarmos um produto que custa R$90,00 em 10% no total 
+        //obteremos R$99,00 como valor final (90 + 9), ao passo que se damos desconto de 10% num produto 
+        //de R$100,00 obteremos R$90,00.
+        //O QUE PODEMOS CONCLUIR?
+            //Que se olhar para o 90 e aumentarmos 10% é diferente de olharmos para o 100 
+            //e descontar 10%. Por isso a porcentagem é uma medida totalmente relativa, pois, 
+            //se tomamos uma porcentagem em relação a valores diferentes, teremos medidas
+            //totalmente diferentes relativas aos valores em questão.
 
-//Como fração é uma razão entre dois números inteiros temos que:
-    //7,5% de 1100, 7,5% é 7,5/100 onde o numerador e denominador pode ser multiplicado 
-    //por 10 que resultado em 75/1000.
-    //Portanto, 7,5/100 equivale 75/1000 que é uma representação válida para essa fração,
-    //que simplificando teremos 3/40.
-    //Portanto 7,5% é equivalente a 3/40.
-    //Se olhar para a fração 7,5/100 vemos que temos que multiplicar 7,5 e dividir por 100, 
-    //então o próprio numerador está te dando o valor que se deve multiplicar e o denominador 
-    //o valor que você tem que dividir. E para todas as porcentagens será assim.
-    //Para tirar 7,5% de 1100 é mais fácil olhar que para 7,5/100 do que para 3/40.
+    //FRAÇÃO É UMA RAZÃO ENTRE NÚMEROS INTEIROS?
+        //Exatamente. Portanto se a fração é uma razão entre dois números inteiros temos que:
+        //7,5% de 1100
+        //7,5% não é inteiro, portanto se queremos olhar para 7,5% como uma fração
+        //devemos multiplicar e dividir o denominador por um mesmo valor até termos uma razão entre inteiros.
+        //Se multiplicamos e divimos por 10, o resultado é 75/1000.
+        //Portanto, 7,5/100 equivale 75/1000 que é uma representação válida para essa fração,
+        //que quando simplificada teremos 3/40.
+        //Portanto 7,5% é equivalente a 3/40.
+        //Se olhar para a fração 7,5/100 vemos que temos que multiplicar 7,5 e dividir por 100, 
+        //então o próprio numerador está te dando o valor que se deve multiplicar e o denominador 
+        //o valor que você tem que dividir. E para todas as porcentagens será assim.
+        //Mesmo sabendo que 3/40 é equivalente a 7,5%, para resolvermos a questão
+        //é mais fácil olharmos para 7,5/100 do que para 3/40.
 
-    //QUAL A OUTRA MANEIRA DE OLHAR?
-        //Se paramos para pensar se divirmos 1100 por 100 obteremos 11, e logo podemos fazer 7,5 * 11 = 82.5.
-        //Bem como poderiamos realizar a conta 7,5/100 * 1100 = 7,5 * 11 = 82.5.
-       
+        //EXISTE OUTRA MANEIRA DE OLHAR?
+            //Se paramos para pensar se divirmos 1100 por 100 obteremos 11, e logo podemos fazer 7,5 * 11 = 82.5.
+            //O resultado será o mesmo se realizamos a conta 7,5/100 * 1100 = 7,5 * 11 = 82.5.
+            //Ou ainda 30/4 * 1100 = 330/4 = 82.5.
+            //Se esse desconto fosse relativo ao pagamento da Previdência, teríamos que o salário líquido da pessoa
+            //é de 1100 reais, o desconto da Previdência é 82,5 e o salário líquido da pessoa seria 1017,5.
+
+    //Entendendo o conceito de porcentagem, responda
+    //COMO UMA PESSOA PODE ESTAR AO MESMO TEMPO ENTRE OS MELHORES E PIORES DE UM JOGO?
+        //Se num jogo temos 10 jogadores, no final da partida temos o placar:
+            //jogador1 = 1000 pontos
+            //jogador2 = 900 pontos
+            //jogador3 = 800 pontos
+            //jogador4 = 700 pontos
+            //jogador5 = 600 pontos
+            //jogador6 = 500 pontos
+            //jogador7 = 400 pontos
+            //jogador8 = 300 pontos
+            //-------eu = 250 pontos -----------
+            //jogador9 = 200 pontos
+            //jogador10 = 100 pontos
+
+            //Em seguida eu entro no jogo e faço 250 pontos.
+        //Logo o jogo percebe que no placar devo ficar entre o jogador9 e o jogador8.
+        //Existe duas formas de olhar minha posição no jogo. 
+        //Se analiso de baixo para cima, temos que fui melhor que 2 jogadores dentre os 10, então:
+            //2/10 = 20/100 = 20%
+        //Se analiso de cima para baixo temos que fui pior que 8 jogadores dentro os 10, então: 
+            //8/10 = 80/100 = 80%
+        //Toda perspectiva é relativa! Podemos dizer tanto que estou dentre os piores jogares quanto
+        //mostrar uma visão mais positiva e dizer que estou entre os 80% melhores jogares, mesmo que
+        //nesse caso não seja tão significativo. Tudo é relativo!
+
+        //Agora pensa se tivermos 854.732 jogares de um jogo qualquer. E fiz 25.000 pontos, porém, 
+        //temos 689.432 com mais de 25.000 pontos. 
+        //COMO COMUNICAR O MEU DESEMPENHO EM RELAÇÃO AO TOTAL DE JOGARES DO JOGO?
+            //Podemos perceber que 689.432/854.732.... representa aproximadamente 0.8066.
+            //Se queremos saber a porcentagem precisamos perguntar: 
+            //QUAL O VALOR QUE PODEMOS INFERIR SE OLHAMOS PARA 0.8066 COM O DENOMINADOR 100?
+            //0.8066/100 = 80,66%.
+            //Isso quer dizer que 80,66% dos jogares fizeram + do que 25.000 pontos. E podemos dizer
+            //que estou entre os 80,66% melhores.     
+            
+            //PS: O número 0.8066 é igual a fração 8066/10000. 10000 porque a cada número após a vírgula 
+            //adicionamos um zero porque estamos represetando a divisão sucessiva por 10, dessa forma temos:
+                //8066/10 = 806,6
+                //806,6/10 = 80,66
+                //80,66/10 = 8,066
+                //8,066/10 = 8066
+                //Foi necessário dividir por 10 4x. É como ter:
+                    // 8066 * 1/10 * 1/10 * 1/10 * 1/10 = 8066/10000.
+                //Essa foi uma curiosidade, mas no exemplo acima não nos interessa a fração, 
+                //apenas queremos saber o quanto 0.8066 representa quando olhamos para ele com o denominador 100,
+                //porque olhar em porcentagem é se importar apenas com o momento que atingimos o denominador 100,
+                //ou seja, 8066 * 1/10 * 1/10 = 80,66%.
+
+            //QUAL É O OUTRO LADO DA MOEDA?
+                //Se 689.432 fizeram mais pontos do que eu, isso quer dizer que 165.300 fizeram menos pontos do que eu.
+                //E de uma maneira similar verificamos a quantos pontos isso corresponde:
+                    //165300/854732 * 100 = 19,34% porque:
+                        //0,1934 que como fração é 1934/10000 que dividindo em cima e em baixo por 100 fica 19,34/100,
+                        //que obviamente é igual a 19,34%.
+                //E uma outra forma de comunicar meu desempenho é dizer que estou entre os 19,34% piores jogadores.            
+                //ERA PRECISO FAZER REALIZAR ESSA CONTA?
+                    //A resposta é não. Uma vez que descobrimos os 80,66% e se sabemos que estamos representando parte
+                    //de um todo, o seu todo é 100%, ou seja, 1 inteiro que é 100/100 = 100%. Então poderíamos apenas
+                    //subtrair do todo os 80,66% para saber qual é a parte que falta:
+                        //100 - 80,66 = 19,34%.
+        //CONCLUSÃO: podemos fazer um eufemismo em dizer que o último jogador está entre os 100% melhores, 
+        //e isso não quer dizer que ele é o melhor. É só uma maneira benevolente de dizer que ele é o pior de todos! :<>
+
+        //PONTO A DESTACAR:
+            //Quando lemos na pandemia, por exemplo, "cresceu o número de hospitalização por caso de COVID em 4300%."
+            //4300 é maior que 100, isso quer dizer que aumentou mais e que não estamos lindando com 1 todo (100%) e sim
+            //que estamos incrementando. 200% seria, por exemplo, 200/100 = 2, quando dizemos que algo cresceu 200% estamos
+            //dizendo que esse valor foi multiplicado por 2, ou seja, dobrou. Então, algo que cresceu 4300%, é dizer 4300/100 = 43
+            //portanto a internação por COVID aumentou em 43x. Esse é um conceito muito importante para nosso comunicação básica
+            //no dia a dia e que muitas pessoas emitem opinião sem nem mesmo compreender isso.
+
+
+
+
+
+    
+        
+        
 
 
 
