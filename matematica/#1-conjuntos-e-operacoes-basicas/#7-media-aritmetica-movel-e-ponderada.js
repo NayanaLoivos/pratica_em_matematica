bb62057942405ecd9dc8db7871444333bff2b7c2
Nayana Loivos <loivosdev@gmail.com>
2023-08-23 15:39:09 -0300

    arithmetic average

---
+++ matematica/#1-conjuntos-e-operacoes-basicas/#7-media-aritmetica-movel-e-ponderada.js
@@ -0,0 +1,46 @@
+//Já vimos fração e porcentagem e sabemos que são meios de conseguir fazer medidas
+//relativas, ou seja, é entender o quanto uma parte representa do todo.
+//As médias nos permite ter uma noção relativa para podermos comparar coisas dentro
+//de uma base comum de valores.
+//Todos esses conceitos de fração, porcentagem e médias são ferramentas importantes dentro
+//do campo da estatística, ou seja, quando estamos lidando com conjuntos de dados, e queremos
+//entender comportamento de coisas a partir de números.
+
+//O QUE É A MÉDIA ARITMÉTICA?
+    //Também chamada de média simples ou apenas média.
+    //A ideia de média é construir um único valor no final que venha a partir dos valores que temos.
+    //A média aritmética tem um problema que é ao somar todos os valores você pode acabar mudando de escala,
+    //pois ao somar todos eles podemos obter valores positivos muito maiores do que tinhamos ou valores negativos
+    //muito menores do que tinhamos. Portanto a ideia é que façamos a divisão pelo mesmo números de valores que somamos.
+    //E assim conseguimos trazer todos esses valores para um escala bem próxima aos valores que já tinhamos.
+    //Por exemplo:
+        console.log((12+7+9+13+8)/5) //9.8
+        //Repara como a escala do resultado final fica próximo dos números que tinhamos.
+        //Seo menor valor é 7 e o maior valor é 13, é natural que essa média esteja entre 7 e 13.
+
+    //PROPRIEDADES DA MÉDIA
+        //Podemos entender algumas propriedades da média somente olhando o resultado:
+        //O resultado não pode ser maior que o maior número que já tinhamos.
+        //O resultado também não pode ser menor que o menor número que já tinhamos.
+        //Sempre vão existir números maiores que puxam a média para cima e menores que puxam a média para baixa, 
+        //e a média ficará vai estar no meio desses valores.
+    
+    //PODEMOS REPRESENTAR A MÉDIA COMO NOTAÇÃO DE FRAÇÃO?
+        //Sim. 
+        console.log((132 +(-22)+76)/3) //62
+        //Dividimos o resultado da soma pelo denominador 3.
+    
+    //CURIOSIDADE 
+        //Se pegamos o PIB do nosso Brasil, não podemos ser enganados a olhar somente o total de riquezas que
+        //o país produziu dentro do ano, devemos pegar esse PIB e dividir essa riqueza pelo total de brasileiros
+        //já que cada brasileiro contribui para esse PIB a partir do seu trabalho. Essa métrica é chamada
+        //de PIB per capita e podemos nos dar conta que o cenário que podia ser bom analisando sobre o PIB, 
+        //passa a apresentar um cenário não tão bom como podia parecer. É importante conhecer os tipos de comparação 
+        //do que estamos falando para obtermos uma base mais adequada.
+
+//O QUE É A MÉDIA PONDERADA?
+
+
+
+
+
